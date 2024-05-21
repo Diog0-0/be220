2e8d95b933f67195496be47cbe07b39b6d49dca7
DiogoSN <dsnogueira489@gmail.com>
2024-05-21 10:56:32 -0300

    new cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent  implements OnInit {
+export class CardComponent implements OnInit {
+  @Input() title: string = "";
+  @Input() label: string = ""
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
     console.log('init')
