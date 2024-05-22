a8b1a7be316a110f7b073a0a58c53dd72a1fc92c
DiogoSN <dsnogueira489@gmail.com>
2024-05-22 19:09:30 -0300

    new type of cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
   @Input() title: string = "";
   @Input() label: string = ""
+  @Input() icon: string = ""
+  @Input() type: string = ""
 
   constructor() {
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  public user: { name: string, level: string } = { name: 'default user', level: 'zero' }
+  public slidesTreinos: { title: string, label: string }[] = []
+  public slidesPersonal: { title: string, label: string, type: string, icon: string }[] = []
 
-  constructor() { }
+  constructor() {
+    this.slidesTreinos = [
+      {
+        title: 'Levantamento de peso',
+        label: 'Continuar treinando',
+      }
+    ];
 
+    this.slidesPersonal = [
+      {
+        title: 'Novo Treino',
+        label: '',
+        type: 'new',
+        icon: 'add-circle-outline'
+      },
+      {
+        title: 'Treino de Costas',
+        label: 'Quarta-Feira',
+        type: 'normal',
+        icon: 'add-circle-outline'
+      }
+    ];
 
+  }
+
+  ngOnInit() {
+    console.log('init')
+  }
+  ngOnDestroy() {
+    console.log('destroy')
+  }
 }
